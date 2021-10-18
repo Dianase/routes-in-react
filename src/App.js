82ff70263319323d78fd6ca7bb5d93280313b4fa
Diana Tineo <diana.tineo1@gmail.com>
2021-10-18 10:39:37 -0400

    setup external components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home"
 import "./App.css";
 
 function App() {
@@ -25,15 +27,19 @@ function App() {
         </nav>
       </header>
       <Switch>
+      <Route exact path='/'>
+        <Home/>
+        </Route>
         <Route path='/contact'>
         <h1>Contact</h1>
         </Route>
         <Route path='/about'>
-        <h1>About</h1>
+        <About/>
         </Route>
         <Route path='/'>
-        <h1>Home</h1>
+        <h1>404- Page not found</h1>
         </Route>
+        
       </Switch>
     </Router>
   );
---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function About(){
+  return(
+    <>
+    <h1>About Us</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus ipsa quisquam sapiente accusantium! Obcaecati repellendus quos laboriosam quia, sequi, hic officia, dolor ea rerum eius minus reiciendis. Magnam, sequi esse.</p>
+    </>
+  )
+}
+
+
+export default About
